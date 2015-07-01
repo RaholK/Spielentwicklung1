85e3dc5dcf64972a1d9626c9bccbcbc67c816446
RaholK <rahol97@live.de>
2015-07-01 17:42:03 +0200

    Create MoveTest.java

---
+++ MoveTest.java
@@ -0,0 +1,37 @@
+//Rahol
+
+package MoveTest;
+
+import javax.swing.JFrame;
+
+public class MoveTest {
+	public static void main(String[] args)  {
+		Frame f = new Frame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(800,600);
+		f.setVisible(true);
+		f.setResizable(false);
+		
+		while(true) {
+			if(f.getUp()) {
+				f.player_posy-=5;
+			}
+			if(f.getDown()) {
+				f.player_posy+=5;
+			}
+			if(f.getRight()) {
+				f.player_posx+= 5;				
+			}
+			if(f.getLeft()) {
+				f.player_posx-=5;
+			}
+			try {
+				Thread.sleep(15);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			f.repaintScreen();
+		}
+	}
+}
